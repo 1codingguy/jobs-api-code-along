f02c398f7b0b1c579589fc6364c1b9dad52f45f4
coding-guy <1codingguy@gmail.com>
2022-09-07 13:29:17 +0100

    create JWT via mongoose instance method

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,11 +6,9 @@ const User = require('../models/User')
 const register = async (req, res) => {
   const user = await User.create({ ...req.body })
 
-  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
-    expiresIn: '30d',
-  })
+  const token = user.createJWT() 
 
-  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
+  res.status(StatusCodes.CREATED).json({ user: { name: user.getName()  }, token })
 }
 const login = async (req, res) => {
   res.send('login user')
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -32,5 +33,11 @@ UserSchema.pre('save', async function () {
   // no need the next() here because of async/await?
   // next()
 })
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, 'jwtSecret', {
+    expiresIn: '30d',
+  })
+}
 
 module.exports = mongoose.model('User', UserSchema)
